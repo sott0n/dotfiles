8e4ae7d8934a13570bbd511f73dfd12636dba24a
Kohei Yamaguchi <yamaguchikuh@nttdata.co.jp>
2017-04-14 11:05:20 +0900

    Add gets.sh and install sh into golang's dotsfiles.

---
+++ go/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GOVERSION=${1}
+if [ -z "${GOVERSION}" ]; then
+    GOVERSION=1.8
+fi
+
+if [ -d ${HOME}/.go/${GOVERSION} ]; then
+    echo "${GOVERSION} is already installed"
+    exit 1
+fi
+
+rm -fr ${GOHOME}/pkg
+mkdir -p ${HOME}/.go/${GOVERSION}
+curl https://storage.googleapis.com/golang/go${GOVERSION}.darwin-amd64.tar.gz \
+    | tar xvzf - -C ${HOME}/.go/${GOVERSION}/ --strip-components=1
+echo $GOVERSION > ${HOME}/.go/.goversion
