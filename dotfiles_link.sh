2eb9edce5660abe8d9c15bd8d41b9dbccb4fae00
Kohei Yamaguchi <yamaguchikuh@nttdata.co.jp>
2017-05-22 11:41:31 +0900

    Update dotsfile anymore.

---
+++ dotfiles_link.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+ln -sf ~/dotfiles/.config ~/.config
+ln -sf ~/dotfiles/.gem ~/.gem
+ln -sf ~/dotfiles/.heroku ~/.heroku
+ln -sf ~/dotfiles/.mozc ~/.mozc
+ln -sf ~/dotfiles/.rvm ~/.rvm
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.profile ~/.profile
