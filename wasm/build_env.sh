80d4a25a8c376ee9ded35ab8dd7dea23f3fe1b23
Kohei Yamaguchi <fix7211@gmail.com>
2020-07-27 20:15:37 +0900

    Add wabt

--- wasm/build_env.sh
+++ wasm/build_env.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+WASM_INSTALL_PATH=$HOME/wasm
+mkdir -p WASM_INSTALL_PATH
+cd ${WASM_INSTALL_PATH}
+
 # Install wasi-libc into clang
 CLANG_PATH=/usr/local/Cellar/llvm/10.0.0_3/lib/clang/10.0.0/lib/
-wget -P /tmp/ https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11-macos.tar.gz
-tar xvf /tmp/wasi-sdk-11-macos.tar.gz
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11-macos.tar.gz
+tar xvf wasi-sdk-11-macos.tar.gz
 
-mkdir ${CLANG_PATH}/wasi
-mv /tmp/wasi-sdk-11-macos/lib/clang/10.0.0/lib/wasi/libclang_rt.builtins-wasm32.a ${CLANG_PATH}/wasi
-rm -rf /tmp/wasi-sdk-11-macos /tmp/wasi-sdk-11-macos.tar.gz
+mkdir -p ${CLANG_PATH}/wasi
+mv wasi-sdk-11-macos/lib/clang/10.0.0/lib/wasi/libclang_rt.builtins-wasm32.a ${CLANG_PATH}/wasi
+rm wasi-sdk-11-macos.tar.gz
 
 # Install wasmtime
 curl https://wasmtime.dev/install.sh -sSf | bash
+
+# Install wabt
+git clone --recursive https://github.com/WebAssembly/wabt; cd wabt
+mkdir build; cd build
+cmake ..
+make -j 8
+
