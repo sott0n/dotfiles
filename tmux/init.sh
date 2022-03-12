5a49c4f5578ead27b91c60d3e15bf20da0ca13d8
Kohei Yamaguchi <fix7211@gmail.com>
2022-03-12 11:41:56 +0900

    Update

--- tmux/init.sh
+++ tmux/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # 
-# See more info at https://github.com/tmux-plugins/tpm
+# See more info at
+#     - https://github.com/tmux-plugins/tpm
+#     - https://github.com/arl/tmux-gitbar           
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/arl/tmux-gitbar.git ~/.tmux-gitbar
