104e86254dd15a4fd74107bdf163d76684dbb118
Kohei Yamaguchi <fix7211@gmail.com>
2023-02-14 09:34:00 +0000

    update

--- python/install.sh
+++ python/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sudo apt install -y zlib1g-dev sqlite3 libssl-dev
+curl https://pyenv.run | bash
+
 PYVERSION=${1}
 if [ -z "${PYVERSION}" ]; then
-    PYVERSION=3.8.0
+    PYVERSION=3.11.1
 fi
 
 if [ -d ${HOME}/.pyenv/versions/${PYVERSION} ]; then
